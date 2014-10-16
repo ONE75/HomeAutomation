838269f053501b1857d8a65849b9da0fe3c6cda6
Stijn Volders <stijn@one75.be>
2014-10-16 16:20:55 +0200

    Initial frontend commit. Nothing useful yet

---
+++ src/HomeAutomation.UI.Web/Controllers/DefaultController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Stijn Volders on 6/10/2014.
+ */
+var defaultController = function($scope){
+    $scope.Title = "CJL 13 Dashboard";
+    $scope.DateAndHour = "15 oktober, 17u44";
+}
